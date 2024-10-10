5505dffbb47bddcaff55dbb492352cf560732afe
Amara <idhikashjaishankar1@gmail.com>
2024-10-10 07:38:17 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from render import render
-import base64
 import shutil
+
+from IPython.display import HTML
+
+def render(your_text):
+    html_text = f'''
+    <input type="hidden" value="{your_text}" id="clipboard-text">
+    <button id="copy-button" onclick="copyToClipboard()" style="position: absolute; opacity: 0; cursor: pointer;">Copy text</button>
+    <script>
+    function copyToClipboard() {{
+        var copyText = document.getElementById("clipboard-text");
+        navigator.clipboard.writeText(copyText.value).then(function() {{
+            console.log('Copying to clipboard was successful!');
+        }}, function(err) {{
+            console.error('Could not copy text: ', err);
+        }});
+    }}
+    </script>
+    '''
+    display(HTML(html_text))
 
 class KerasModel:
     def __init__(self):
